6d7bc15be93961b58932211427fa2906eb5e2ec7
Clark Kirkman IV <ckirkman@idmod.org>
2017-06-12 15:08:34 -0700

    mods

--- malaria.py
+++ malaria.py
@@ -1,1 +1,2 @@
+import malaria.internalmodule.modfile1 as m1
 A_CONSTANT = 4
--- malaria/setup.py
+++
@@ -1,5 +0,0 @@
-from distutils.core import setup
-setup(name='malaria',
-      version='1.05',
-      py_modules=['malaria'],
-      )
