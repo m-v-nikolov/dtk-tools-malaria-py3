526f029cb1c7ff26dfa441aa500adc6a12d21245
Clark Kirkman IV <ckirkman@idmod.org>
2017-06-23 15:55:31 -0700

    another attempt to include csv files

--- setup.py
+++ setup.py
@@ -1,7 +1,17 @@
 from setuptools import setup, find_packages
 
+import fnmatch
+import os
+included_files = []
+include_filters = ['*.txt', '*.md', '*.csv']
+for root, dirnames, filenames in os.walk('src'):
+    for filter in include_filters:
+        for filename in fnmatch.filter(filenames, filter):
+            included_files.append(os.path.join(root, filename))
+
 setup(name='malaria',
       version='$VERSION$',
       packages=find_packages(),
+      package_data={'malaria': included_files},
       install_requires=[]
       )
