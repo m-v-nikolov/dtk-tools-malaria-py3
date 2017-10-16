2b10ee317466407ea1cec76dc08194dec2263c43
Benoit Raybaud <braybaud@intven.com>
2017-10-16 08:37:14 -0700

    Python 3.6 compliance

--- malaria/params.py
+++ malaria/params.py
@@ -1,8 +1,6 @@
 import copy
 
-import infection
-import immunity
-import symptoms
+from malaria import infection, immunity, symptoms
 from dtk.vector.species import set_params_by_species
 from malaria.interventions.malaria_drugs import drug_params
 
