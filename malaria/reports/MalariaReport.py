db1506cb6beae5dcca8a58b6bb0499d690d3d3bd
Benoit Raybaud <braybaud@intven.com>
2017-11-08 11:52:05 -0800

    remove np.inf for sys.maxsize

--- malaria/reports/MalariaReport.py
+++ malaria/reports/MalariaReport.py
@@ -1,3 +1,4 @@
+import sys
 from dtk.utils.reports.CustomReport import BaseReport, BaseEventReport, BaseEventReportIntervalOutput
 import numpy as np
 
@@ -35,7 +36,7 @@ class MalariaReport(BaseEventReportIntervalOutput):
 
 default_age_bins = [1.0 / 12, 0.5, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
                     11, 12, 13, 14, 15, 20, 25, 30, 40, 50, 60, 1000]
-default_parasitemia_bins = [50.0, 500.0, 5000.0, np.inf]
+default_parasitemia_bins = [50.0, 500.0, 5000.0, sys.maxsize]
 default_infection_bins = [20.0, 40.0, 60.0, 80.0, 100.0]
 
 
